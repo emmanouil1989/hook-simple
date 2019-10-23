3fd6533006e0f7b6233408ef149e8e6d1c8ddee0
emmanouile <manos.emmanouil@deliveryblueprints.com>
2019-10-23 09:06:17 +0100

    1) refactor rescource list to use the 'useEffect' 2) make api call and update local state 3) create a list of titles

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios';
 
-const ResourceList = () => {
+const ResourceList = (props) => {
+    const [resources, setResources] = useState([]);
+    const {resource} = props;
 
+    //use effect doen't like promises called inside it. it should be normal functions. another solution is to define a function with the async call and immediately call it.
+    useEffect(()=>{
+        fetchResource(resource);
 
-    const fetchResource = async ()  => {
-        const response = await axios.get(`https://jsonplaceholder.typecode.com/${this.props.resource}`);
-        this.setState({resources: response.data})
+    },[resource]);
 
+    const fetchResource = async (resource)  => {
+        const response =  await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+        setResources(response.data)
     };
 
     return(
-        <div>{this.props.resources.length}</div>
+        <ul>
+            {
+                resources.map(record =>(
+                    <li key={record.id}> {record.title }</li>
+                ))
+            }
+        </ul>
     )
 };
 
